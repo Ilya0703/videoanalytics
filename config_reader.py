c028c002e69591aef000392647ab5d1a3303ad0b
Ilya0703 <trifonov07032004@gmail.com>
2025-05-21 15:45:36 +0300

    default values added

--- config_reader.py
+++ config_reader.py
@@ -1,7 +1,5 @@
-import ast
-
 import yaml
-import numpy as np
+
 class ConfigReader:
     def __init__(self, config_path):
         self.config_path = config_path
@@ -17,21 +15,21 @@ class ConfigReader:
         with open(self.config_path) as f:
             depth_map_parameters = yaml.safe_load(f)["object_detector_parameters"]
         if "confidence" not in depth_map_parameters.keys():
-            return ""
+            return 0.5
         return depth_map_parameters['confidence']
 
     def motion_detector_min_motion_area(self):
         with open(self.config_path) as f:
             depth_map_parameters = yaml.safe_load(f)["motion_detector_parameters"]
         if "min_motion_area" not in depth_map_parameters.keys():
-            return ""
+            return 200
         return depth_map_parameters['min_motion_area']
 
     def server_host(self):
         with open(self.config_path) as f:
             depth_map_parameters = yaml.safe_load(f)["server_parameters"]
         if "host" not in depth_map_parameters.keys():
-            return ""
+            return 5000
         return depth_map_parameters['host']
 
     def cameras_dict(self):
